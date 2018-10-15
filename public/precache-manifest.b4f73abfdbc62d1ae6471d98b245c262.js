406426b60e14ed242ab6d57af328ce1504ce0241
joebit213 <joseph@evatic.org>
2018-10-15 14:22:19 -0500

    keroku

---
+++ public/precache-manifest.b4f73abfdbc62d1ae6471d98b245c262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df22fd301050f2193e30",
+    "url": "/static/js/main.df22fd30.chunk.js"
+  },
+  {
+    "revision": "975cf1d50f5e44bf6533",
+    "url": "/static/js/1.975cf1d5.chunk.js"
+  },
+  {
+    "revision": "df22fd301050f2193e30",
+    "url": "/static/css/main.b4e682ad.chunk.css"
+  },
+  {
+    "revision": "975cf1d50f5e44bf6533",
+    "url": "/static/css/1.269f6379.chunk.css"
+  },
+  {
+    "revision": "49a6f3898a91c1869313249211389988",
+    "url": "/index.html"
+  }
+];
